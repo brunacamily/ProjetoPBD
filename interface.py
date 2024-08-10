85193cfddd36cb3556283c533f4a34f361e76ff9
BrunaCamily <brunacamilydomingues@gmail.com>
2024-08-10 12:34:39 -0300

    Bruna:inseri o menu

--- interface.py
+++ interface.py
@@ -1,213 +1,185 @@
 import psycopg2
+from psycopg2 import sql
 
-# Configurações do banco de dados
-db_config = {
-    'host': 'localhost',
-    'database': 'projetopbd',
-    'user': 'postgres',
-    'password': '26022004',
-    'port': '5432'
-}
+def cadastra_user(cursor, nome, email, cpf):
+    cursor.execute("INSERT INTO projetopbd.usuario (nome, email, cpf) VALUES (%s, %s, %s)", (nome, email, cpf))
 
-# Função para conectar ao banco de dados
-def connect():
-    try:
-        conn = psycopg2.connect(**db_config)
-        conn.set_client_encoding('UTF8')  # Defina a codificação do cliente
-        print("Conectado ao banco de dados com sucesso!")
-        return conn
-    except Exception as e:
-        print(f"Erro ao conectar ao banco de dados: {e}")
-        return None
+def cadastra_porto(cursor, cidade, tipo):
+    cursor.execute(
+        "INSERT INTO porto (cidade, tipo) VALUES (%s, %s)", 
+        (cidade, tipo)
+    )
 
-# Funções para operar em cada tabela
+def cadastra_administrador(cursor, user_adm, password_adm):
+    return cursor.execute(
+        "INSERT INTO projetopbd.administrador (id_adm, user_adm, password_adm) VALUES (12,%s, %s)", 
+        (user_adm, password_adm)
+    )
 
-# Administrador
-def add_administrador(conn, id_adm, user_adm, password_adm):
-    try:
-        cur = conn.cursor()
-        cur.execute('''
-            INSERT INTO administrador (id_adm, user_adm, password_adm)
-            VALUES (%s, %s, %s)
-        ''', (id_adm, user_adm, password_adm))
-        conn.commit()
-        cur.close()
-        print("Administrador adicionado com sucesso!")
-    except Exception as e:
-        print(f"Erro ao adicionar administrador: {e}")
+def cadastra_navio(cursor, toneladas, status, localizacao):
+    cursor.execute(
+        "INSERT INTO projetopbd.navio (toneladas, status, localizacao) VALUES (%s, %s, %s)", 
+        (toneladas, status, localizacao)
+    )
 
-# Usuário
-def add_usuario(conn, id_user, nome, email, cpf, cnpj):
-    try:
-        cur = conn.cursor()
-        cur.execute('''
-            INSERT INTO usuario (id_user, nome, email, cpf, cnpj)
-            VALUES (%s, %s, %s, %s, %s)
-        ''', (id_user, nome, email, cpf, cnpj))
-        conn.commit()
-        cur.close()
-        print("Usuário adicionado com sucesso!")
-    except Exception as e:
-        print(f"Erro ao adicionar usuário: {e}")
+##Verificar se navio esta livre na data da viagem
+#def cria_viagem(cursor, data_partida, hora_partida, data_chegada, hora_chegada, peso_carga, id_origem, id_destino, id_navio, id_adm):
+#    cursor.execute(
+#        """
+#        INSERT INTO viagem (data_partida, hora_partida, data_chegada, hora_chegada, peso_carga, id_origem, id_destino, id_navio, id_adm) 
+#        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+#        """, 
+#        (data_partida, hora_partida, data_chegada, hora_chegada, peso_carga, id_origem, id_destino, id_navio, id_adm)
+#    )
 
-# Porto
-def add_porto(conn, id_porto, cidade, tipo):
-    try:
-        cur = conn.cursor()
-        cur.execute('''
-            INSERT INTO porto (id_porto, cidade, tipo)
-            VALUES (%s, %s, %s)
-        ''', (id_porto, cidade, tipo))
-        conn.commit()
-        cur.close()
-        print("Porto adicionado com sucesso!")
-    except Exception as e:
-        print(f"Erro ao adicionar porto: {e}")
+def finalizar_viagem(cursor, id_viagem, data_chegada):
+    cursor.execute(f"UPDATE projetopbd.viagem A SET data_chegada = '{data_chegada}' WHERE A.id_viagem = {id_viagem}")
 
-# Navio
-def add_navio(conn, id_navio, toneladas, status, localizacao):
-    try:
-        cur = conn.cursor()
-        cur.execute('''
-            INSERT INTO navio (id_navio, toneladas, status, localizacao)
-            VALUES (%s, %s, %s, %s)
-        ''', (id_navio, toneladas, status, localizacao))
-        conn.commit()
-        cur.close()
-        print("Navio adicionado com sucesso!")
-    except Exception as e:
-        print(f"Erro ao adicionar navio: {e}")
+def historico_todas_viagens(cursor):
+    cursor.execute("SELECT * FROM projetopbd.viagem")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+        
+#Verifica se o navio estara ocupado em certo intervalo de datas
+#Retorna true se esta livre
+def verificar_status_navio(cursor, id_navio, data_partida, data_chegada):
+    cursor.execute(f" select * from projetopbd.viagem B inner join projetopbd.navio A on B.id_navio = A.id_navio and B.id_navio = '{id_navio}' and B.data_partida >= '{data_partida}' and B.data_chegada <= '{data_chegada}'")
+    rows = cursor.fetchall()
+    print(rows)
+    return rows == []
 
-# Viagem
-def add_viagem(conn, id_viagem, data_partida, hora_partida, data_chegada, hora_chegada, peso_carga, id_origem, id_destino, id_navio, id_adm):
-    try:
-        cur = conn.cursor()
-        cur.execute('''
-            INSERT INTO viagem (id_viagem, data_partida, hora_partida, data_chegada, hora_chegada, peso_carga, id_origem, id_destino, id_navio, id_adm)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        ''', (id_viagem, data_partida, hora_partida, data_chegada, hora_chegada, peso_carga, id_origem, id_destino, id_navio, id_adm))
-        conn.commit()
-        cur.close()
-        print("Viagem adicionada com sucesso!")
-    except Exception as e:
-        print(f"Erro ao adicionar viagem: {e}")
+#Printa o status de toda a frota
+def verificar_status_frota_completa(cursor):
+    cursor.execute("SELECT * FROM projetopbd.navio")
+    rows = cursor.fetchall()
+    for i in rows:
+        print(f"Navio {i[0]} está {i[2]}")
+    return
 
-# Container
-def add_container(conn, id_container, peso, conteudo, nota_fiscal, tipo, id_usuario):
-    try:
-        cur = conn.cursor()
-        cur.execute('''
-            INSERT INTO container (id_container, peso, conteudo, nota_fiscal, tipo, id_usuario)
-            VALUES (%s, %s, %s, %s, %s, %s)
-        ''', (id_container, peso, conteudo, nota_fiscal, tipo, id_usuario))
-        conn.commit()
-        cur.close()
-        print("Container adicionado com sucesso!")
-    except Exception as e:
-        print(f"Erro ao adicionar container: {e}")
+#Printa todas os id de viagens que o locatario já fez
+def historico_alugueis_de_user(cursor, id_usuario):
+#select (E.id_viagem,E.data_chegada, E.data_partida, F.cidade, H.cidade) from projetopbd.usuario A
+#inner join projetopbd.container B on B.id_usuario = A.id_user
+#inner join projetopbd.transporta D on B.id_container = D.id_container 
+#inner join projetopbd.viagem E on D.id_viagem = E.id_viagem
+#-- Tabela F para origem
+#inner join projetopbd.porto F on E.id_origem = F.id_porto  
+#-- Tabela H para destino
+#inner join projetopbd.porto H on E.id_destino = H.id_porto
+#and A.id_user = 1
+#--inner join projetopbd.viagem C on C.id_container = B.id_container  
 
-# Transporte
-def add_transporta(conn, id_viagem, id_container, num_container):
-    try:
-        cur = conn.cursor()
-        cur.execute('''
-            INSERT INTO transporta (id_viagem, id_container, num_container)
-            VALUES (%s, %s, %s)
-        ''', (id_viagem, id_container, num_container))
-        conn.commit()
-        cur.close()
-        print("Transporte adicionado com sucesso!")
-    except Exception as e:
-        print(f"Erro ao adicionar transporte: {e}")
+    cursor.execute(f"select id_container from projetopbd.usuario A inner join projetopbd.container B on A.id_user = B.id_usuario and A.id_user = '{id_usuario}'")
+    
+    rows = cursor.fetchall()
+    for row in rows:
+        cursor.execute(f"select id_viagem from projetopbd.transporta A inner join projetopbd.container B on A.id_container = B.id_container and A.id_container = '{row[0]}'")
+        print(cursor.fetchall())
 
-# Consultar Tabelas
-def query_table(conn, table_name):
-    try:
-        cur = conn.cursor()
-        cur.execute(f'SELECT * FROM {table_name}')
-        rows = cur.fetchall()
-        cur.close()
-        for row in rows:
-            print(row)
-    except Exception as e:
-        print(f"Erro ao consultar tabela {table_name}: {e}")
+def criar_viagem(cursor, data_partida, hora_partida, data_chegada, hora_chegada, peso_carga, id_origem, id_destino, id_adm):
+    navios_livres = []
+    cursor.execute(f"select id_navio from projetopbd.navio ")
+    rows = cursor.fetchall()
+    for row in rows:
+        if verificar_status_navio(cursor, row[0], data_partida, data_chegada):
+            navios_livres.append(row[0])
+    cursor.excecute(f"insert into projetopbd.viagem (data_partida, hora_partida, data_chegada, hora_chegada, peso_carga, id_origem, id_destino, id_navio, id_adm) values ({data_partida}, {hora_partida}, {data_chegada}, {hora_chegada}, {peso_carga}, {id_origem}, {id_destino}, {navios_livres[0]}, {id_adm})")
 
-# Interface de Usuário
-def main():
-    conn = connect()
-    if conn:
-        while True:
-            print("\nMenu:")
-            print("1. Adicionar Administrador")
-            print("2. Adicionar Usuário")
-            print("3. Adicionar Porto")
-            print("4. Adicionar Navio")
-            print("5. Adicionar Viagem")
-            print("6. Adicionar Container")
-            print("7. Adicionar Transporte")
-            print("8. Consultar Tabela")
-            print("9. Sair")
+def main_menu(cursor):
+    while True:
+        print("\nMenu:")
+        print("1. Cadastrar Usuário")
+        print("2. Cadastrar Porto")
+        print("3. Cadastrar Administrador")
+        print("4. Cadastrar Navio")
+        print("5. Finalizar Viagem")
+        print("6. Verificar Status de um Navio")
+        print("7. Verificar Status da Frota Completa")
+        print("8. Histórico de Todas as Viagens")
+        print("9. Histórico de Aluguéis de um Usuário")
+        print("10. Criar Viagem")
+        print("0. Sair")
+        escolha = input("Escolha uma opção: ")
 
-            choice = input("Escolha uma opção: ")
+        if escolha == "1":
+            nome = input("Nome: ")
+            email = input("Email: ")
+            cpf = input("CPF: ")
+            cadastra_user(cursor, nome, email, cpf)
 
-            if choice == '1':
-                id_adm = int(input("ID do Administrador: "))
-                user_adm = input("Usuário: ")
-                password_adm = input("Senha: ")
-                add_administrador(conn, id_adm, user_adm, password_adm)
-            elif choice == '2':
-                id_user = int(input("ID do Usuário: "))
-                nome = input("Nome: ")
-                email = input("Email: ")
-                cpf = input("CPF (opcional): ")
-                cnpj = input("CNPJ (opcional): ")
-                add_usuario(conn, id_user, nome, email, cpf, cnpj)
-            elif choice == '3':
-                id_porto = int(input("ID do Porto: "))
-                cidade = input("Cidade: ")
-                tipo = input("Tipo (refrigerado / não refrigerado): ")
-                add_porto(conn, id_porto, cidade, tipo)
-            elif choice == '4':
-                id_navio = int(input("ID do Navio: "))
-                toneladas = int(input("Toneladas: "))
-                status = input("Status (em viagem / livre): ")
-                localizacao = input("Localização: ")
-                add_navio(conn, id_navio, toneladas, status, localizacao)
-            elif choice == '5':
-                id_viagem = int(input("ID da Viagem: "))
-                data_partida = input("Data de Partida: ")
-                hora_partida = input("Hora de Partida: ")
-                data_chegada = input("Data de Chegada: ")
-                hora_chegada = input("Hora de Chegada: ")
-                peso_carga = int(input("Peso da Carga: "))
-                id_origem = int(input("ID do Porto de Origem: "))
-                id_destino = int(input("ID do Porto de Destino: "))
-                id_navio = int(input("ID do Navio: "))
-                id_adm = int(input("ID do Administrador: "))
-                add_viagem(conn, id_viagem, data_partida, hora_partida, data_chegada, hora_chegada, peso_carga, id_origem, id_destino, id_navio, id_adm)
-            elif choice == '6':
-                id_container = int(input("ID do Container: "))
-                peso = int(input("Peso: "))
-                conteudo = input("Conteúdo: ")
-                nota_fiscal = input("Nota Fiscal: ")
-                tipo = input("Tipo (refrigerado / não refrigerado): ")
-                id_usuario = int(input("ID do Usuário: "))
-                add_container(conn, id_container, peso, conteudo, nota_fiscal, tipo, id_usuario)
-            elif choice == '7':
-                id_viagem = int(input("ID da Viagem: "))
-                id_container = int(input("ID do Container: "))
-                num_container = int(input("Número do Container: "))
-                add_transporta(conn, id_viagem, id_container, num_container)
-            elif choice == '8':
-                table_name = input("Nome da Tabela: ")
-                query_table(conn, table_name)
-            elif choice == '9':
-                print("Saindo...")
-                break
-            else:
-                print("Opção inválida. Tente novamente.")
+        elif escolha == "2":
+            cidade = input("Cidade: ")
+            tipo = input("Tipo (refrigerado/não refrigerado): ")
+            cadastra_porto(cursor, cidade, tipo)
 
-        conn.close()
+        elif escolha == "3":
+            user_adm = input("Nome do Administrador: ")
+            password_adm = input("Senha: ")
+            cadastra_administrador(cursor, user_adm, password_adm)
 
-if __name__ == "__main__":
-    main()
+        elif escolha == "4":
+            toneladas = input("Toneladas: ")
+            status = input("Status (em viagem/livre): ")
+            localizacao = input("Localização: ")
+            cadastra_navio(cursor, toneladas, status, localizacao)
+
+        elif escolha == "5":
+            id_viagem = input("ID da Viagem: ")
+            data_chegada = input("Data de Chegada: ")
+            finalizar_viagem(cursor, id_viagem, data_chegada)
+
+        elif escolha == "6":
+            id_navio = input("ID do Navio: ")
+            data_partida = input("Data de Partida: ")
+            data_chegada = input("Data de Chegada: ")
+            verificar_status_navio(cursor, id_navio, data_partida, data_chegada)
+
+        elif escolha == "7":
+            verificar_status_frota_completa(cursor)
+
+        elif escolha == "8":
+            historico_todas_viagens(cursor)
+
+        elif escolha == "9":
+            id_usuario = input("ID do Usuário: ")
+            historico_alugueis_de_user(cursor, id_usuario)
+
+        elif escolha == "10":
+            data_partida = input("Data de Partida: ")
+            hora_partida = input("Hora de Partida: ")
+            data_chegada = input("Data de Chegada: ")
+            hora_chegada = input("Hora de Chegada: ")
+            peso_carga = input("Peso da Carga: ")
+            id_origem = input("ID de Origem: ")
+            id_destino = input("ID de Destino: ")
+            id_adm = input("ID do Administrador: ")
+            criar_viagem(cursor, data_partida, hora_partida, data_chegada, hora_chegada, peso_carga, id_origem, id_destino, id_adm)
+
+        elif escolha == "0":
+            break
+
+        else:
+            print("Opção inválida. Tente novamente.")
+
+try:
+    conn = psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password="26022004",
+        host="localhost",
+        port="5432"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("Você está conectado ao - ", record, "\n")
+
+    main_menu(cursor)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+except (Exception, psycopg2.Error) as error:
+    print("Erro ao conectar ao PostgreSQL", error)
--- projetopbd.sql
+++ projetopbd.sql
@@ -63,7 +63,41 @@ CREATE TABLE transporta(
 	FOREIGN KEY (id_viagem) REFERENCES viagem(id_viagem),
 	FOREIGN KEY (id_container) REFERENCES container(id_container));
 
+-- Inserindo dados na tabela administrador
+INSERT INTO administrador VALUES (3, 'Julia', '4321');
 insert into administrador values (01,'Maria', 1234);
+
+-- Inserindo dados na tabela usuario
+INSERT INTO usuario VALUES 
+(1, 'João Silva', 'joao.silva@email.com', '123.456.789-00', NULL),
+(2, 'Empresa XYZ', 'contato@xyz.com.br', NULL, '12.345.678/0001-00');
+
+
+-- Inserindo dados na tabela porto
+INSERT INTO porto VALUES 
+(1, 'Rio de Janeiro', 'refrigerado'),
+(2, 'Santos', 'não refrigerado');
+
+-- Inserindo dados na tabela navio
+INSERT INTO navio VALUES 
+(1, 10000, 'em viagem', 'Atlântico'),
+(2, 5000, 'livre', 'Porto de Santos');
+
+-- Inserindo dados na tabela viagem
+INSERT INTO viagem VALUES 
+(1, '2024-08-01', '08:00', '2024-08-10', '14:00', 8000, 1, 2, 1, 1),
+(2, '2024-08-05', '09:00', '2024-08-15', '16:00', 3000, 2, 1, 2, 1);
+
+-- Inserindo dados na tabela container
+INSERT INTO container VALUES 
+(1, 1000, 'Alimentos perecíveis', 'NF-123456', 'refrigerado', 1),
+(2, 2000, 'Eletrônicos', 'NF-654321', 'não refrigerado', 2);
+
+-- Inserindo dados na tabela transporta
+INSERT INTO transporta VALUES 
+(1, 1, 1),
+(2, 2, 1);
+
 
  SELECT * FROM administrador;
 
